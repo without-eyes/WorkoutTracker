ea3d4b23d1cf8bf578a885558f5799eabd3d6b7f
without eyes <37347347+without-eyes@users.noreply.github.com>
2025-03-15 11:20:49 +0200

    Update UserController

--- src/main/java/com/workouttracker/rest/controller/WorkoutController.java
+++ src/main/java/com/workouttracker/rest/controller/WorkoutController.java
@@ -2,33 +2,31 @@ package com.workouttracker.rest.controller;
 
 import org.springframework.web.bind.annotation.*;
 import com.workouttracker.rest.model.Workout;
+import com.workouttracker.service.WorkoutService;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/workouts")
 public class WorkoutController {
-    private Map<Long, Workout> workoutsMap = new HashMap<>();
+    private final WorkoutService workoutService;
+
+    public WorkoutController(WorkoutService workoutService) {
+        this.workoutService = workoutService;
+    }
 
     @GetMapping
-    public Collection<Workout> getAllWorkouts() {
-        return workoutsMap.values();
+    public List<Workout> getAllWorkouts() {
+        return workoutService.getAllWorkouts();
     }
 
     @PostMapping
-    public void addWorkout(@RequestBody Workout workout) {
-        workoutsMap.put(workout.getId(), workout);
-    }
-
-    @PutMapping("/{id}")
-    public void updateWorkout(@PathVariable Long id, @RequestBody Workout workout) {
-        workoutsMap.put(id, workout);
+    public Workout addWorkout(@RequestBody Workout workout) {
+        return workoutService.saveWorkout(workout);
     }
 
     @DeleteMapping("/{id}")
     public void deleteWorkout(@PathVariable Long id) {
-        workoutsMap.remove(id);
+        workoutService.deleteWorkout(id);
     }
 }
