ce256d965a5d4133a26d85b3026b29e3a3bd7794
without eyes <37347347+without-eyes@users.noreply.github.com>
2025-03-15 10:59:14 +0200

    Add UserService

---
+++ src/main/java/com/workouttracker/service/UserService.java
@@ -0,0 +1,31 @@
+package com.workouttracker.service;
+
+import com.workouttracker.rest.model.User;
+import com.workouttracker.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
