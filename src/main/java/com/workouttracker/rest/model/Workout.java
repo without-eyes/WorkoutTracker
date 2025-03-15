cab828b64a49113fc2aac699c1afc810bab88443
without eyes <37347347+without-eyes@users.noreply.github.com>
2025-03-15 10:39:17 +0200

    Add Workout model

---
+++ src/main/java/com/workouttracker/rest/model/Workout.java
@@ -0,0 +1,33 @@
+package com.workouttracker.rest.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "Workouts")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Workout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id")
+    private int id;
+
+    @Column(name = "UserId")
+    private int userId;
+
+    @Column(name = "Type")
+    private String type;
+
+    @Column(name = "DurationInMinutes")
+    private int duration;
+
+    @Column(name = "Date")
+    private LocalDateTime date;
+
+}
